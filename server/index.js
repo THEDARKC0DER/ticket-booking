ad45ea19cad0d69bc22c4167e5733871a87170de
Pegasus321 <ayush.agrawal@vit.edu.in>
2025-05-23 23:33:41 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,16 +1,26 @@
-const express = require('express')
+const express = require("express");
 const path = require("path");
-var cors = require('cors')
+var cors = require("cors");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const dbConfig = require('./config/dbConfig')
+const dbConfig = require("./config/dbConfig");
 
+const app = express();
 
+const userRoutes = require("./routes/userRoutes");
+const movieRoute = require("./routes/movieRoutes");
+const theatreRoute = require("./routes/theatreRoute");
+const showRoute = require("./routes/showRoute");
+const bookingRoute = require("./routes/bookingRoute");
 
-
-const app = express()
-
+app.use(cors());
+app.use(express.json());
+app.use("/api/users", userRoutes);
+app.use("/api/movies", movieRoute);
+app.use("/api/theatres", theatreRoute);
+app.use("/api/shows", showRoute);
+app.use("/api/bookings", bookingRoute);
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
@@ -18,31 +28,8 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-const userRoutes = require('./routes/userRoutes')
-const movieRoute = require('./routes/movieRoutes')
-const theatreRoute = require('./routes/theatreRoute')
-const showRoute = require('./routes/showRoute')
-const bookingRoute = require('./routes/bookingRoute')
+const PORT = process.env.PORT || 8081;
 
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/users' , userRoutes)
-app.use('/api/movies' , movieRoute)
-app.use('/api/theatres' , theatreRoute )
-app.use('/api/shows' , showRoute )
-app.use('/api/bookings' , bookingRoute )
-
-
-const PORT = process.env.PORT || 8081
-
-
-
-
-app.listen(PORT , ()=>{
-    console.log("server running")
-})
-
-
-
-
+app.listen(PORT, () => {
+  console.log("server running");
+});
